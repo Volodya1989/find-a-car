cd253aceac1dca6a5275521f569a7d588ea3cafe
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2023-11-18 20:06:40 -0500

    version one for card

--- src/components/Card/Card.styled.js
+++ src/components/Card/Card.styled.js
@@ -1,1 +1,52 @@
+import styled from 'styled-components';
 
+export const CardWrapper = styled.li`
+  box-shadow: 0px 2px 1px 0px rgba(46, 47, 66, 0.08);
+  border-radius: 6px;
+  width: 264px;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  border-radius: 10px;
+`;
+
+export const CardSection = styled.div`
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 254px;
+  text-align: center;
+  padding-top: 32px;
+  padding-bottom: 32px;
+  background-color: #ffffff;
+  border-bottom-left-radius: 4px;
+  border-bottom-right-radius: 4px;
+  text-align: left;
+  background-color: gray;
+  padding-left: 10px;
+`;
+
+export const CardTitle = styled.h5`
+  color: #2e2f42;
+  font-weight: 500;
+  line-height: 1.2;
+  margin-top: 0;
+  margin-bottom: 0;
+`;
+
+export const CardDescription = styled.p`
+  margin-bottom: 8px;
+  font-size: 22px;
+  margin-top: 0;
+  margin-bottom: 0;
+`;
+export const CardImage = styled.img`
+  display: block;
+  max-width: 100%;
+  margin-left: auto;
+  margin-right: auto;
+  width: 264px;
+  height: 260px;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+`;
---
+++ src/styles/variables.js
@@ -0,0 +1,3 @@
+export const containerSize = {
+  fromL: 1500,
+};
