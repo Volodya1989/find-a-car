9a33a85d3ff36a5337921183d6ccaa83f31ff720
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2023-11-20 11:02:44 -0500

    loader version 3

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -9,7 +9,7 @@ const Loader = () => {
         width="100"
         color="#63d1f2"
         margin="auto"
-        wrapperStyle={{ marginTop: '15%' }}
+        wrapperStyle={{ marginTop: '10%' }}
         wrapperClass=""
         visible={true}
         ariaLabel="three-circles-rotating"
