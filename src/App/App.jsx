7e1171d2ad0b0734a648ef938ac6419dc3ff0349
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2023-11-26 01:43:15 -0500

    navigate removed

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,5 +1,5 @@
 import { lazy } from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import SharedLayout from '../components/SharedLayout';
 
 const Home = lazy(() => import('../Pages/Home'));
